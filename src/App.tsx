f7bc9732c321c502460df68955bb5709cea5649b
Boyd_Thurston <boydthurston@gmail.com>
2021-06-22 14:20:21 +1200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import clsx from 'clsx'
+import { makeStyles, AppBar, Tab, Tabs, Slide, Typography } from '@material-ui/core'
+
+const useStyles = makeStyles(theme => ({
+  text: {
+    fontSize: 200,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    textAlign: 'center',
+    color: '#FFFFFF',
+  },
+  page: {
+    width: '100%',
+    height: '100vh',
+  },
+  pageOne: {
+    color: 'black'
+  },
+  pageTwo: {
+    backgroundColor: 'blue'
+  },
+  pageThree: {
+    backgroundColor: 'purple'
+  },
+}))
 
 function App() {
+  const classes = useStyles()
+  const [pageNumber, setPageNumber] = useState(0)
+
+  const handleTabChange = (event: any, newValue: number) => {
+    setPageNumber(newValue)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main >
+      <AppBar position='static'>
+        <Tabs value={pageNumber} onChange={handleTabChange}>
+          <Tab label='Page One'/>
+          <Tab label='Page Two'/>
+          <Tab label='Page Three'/>
+        </Tabs>
+      </AppBar>
+        <div hidden={pageNumber !== 0} className={classes.page}>
+          <Typography className={clsx(classes.text, classes.pageOne)}>one</Typography>
+        </div>
+      <Slide 
+        direction="right" 
+        in={pageNumber == 1} 
+        timeout={700} 
+        mountOnEnter 
+        unmountOnExit
+      >
+        <div className={clsx(classes.page, classes.pageTwo)}>
+          <Typography className={classes.text}>two</Typography>
+        </div>
+      </Slide>
+      <Slide direction="right" in={pageNumber == 2} mountOnEnter unmountOnExit>
+        <div className={clsx(classes.page, classes.pageThree)}>
+          <Typography className={classes.text}>three</Typography>
+        </div>
+      </Slide>
+    </main>
   );
 }
 
